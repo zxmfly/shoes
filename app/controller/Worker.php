025e35479e19e82fb235ff97c3638e8fdc7f4602
zxmfly <zenghuayuan@126.com>
2020-06-05 00:09:19 +0800

    【修改】员工列表：添加员工；

--- app/controller/Worker.php
+++ app/controller/Worker.php
@@ -22,12 +22,37 @@ class Worker extends BaseAdmin
     public function getList(){
         $keywords = Request::get('keywords');
         $count = Users::count();
-        $data = Users::select()->toArray();
+        $data = [];
+        if($count) {
+            $data = Users::select()->toArray();
+        }
         $code = 0;
         $msg = 'ok';
         $list = compact('code','msg','count','data');
 
         return json($list);
+    }
+
+    public function addWorker(){
+        if(Request::method() == 'POST'){
+            $data = Request::param();
+            $user = Users::where('user_name', $data['user_name'])
+                ->whereOr('name', $data['name'])
+                ->whereOr('number', $data['number'])
+                ->find();
+            if($user){
+                return json($data);
+            }
+            $data['update_time'] = 0;
+            $data['create_time'] = time();
+            $data['password'] = $data['password'] ? $data['password'] : '123';
+            $data['password'] = md5($data['user_name'].$data['password']);
+            $insert = Users::insert($data);
+            $res = $insert ? ['code'=>0,'msg'=>'添加成功'] : ['code'=>1,'msg'=>'添加失败'];
+            return json($res);
+        }
+
+        return View::fetch();
     }
 
     public function work(){
